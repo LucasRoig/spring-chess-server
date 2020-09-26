4d07eb2cecc9a192de141e93ff2d11c9596e46c9
Lucas Roig <lucas1.roig@gmail.com>
2020-09-26 23:11:17 +0200

    war packaging

--- src/main/java/fr/athome/chessserver/ChessServerApplication.java
+++ src/main/java/fr/athome/chessserver/ChessServerApplication.java
@@ -5,6 +5,7 @@ import fr.athome.chessserver.chess.pgn.PgnReader;
 import fr.athome.chessserver.dbchessentity.DbGameFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -14,7 +15,7 @@ import java.util.List;
 
 
 @SpringBootApplication
-public class ChessServerApplication {
+public class ChessServerApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) throws IOException {
         SpringApplication.run(ChessServerApplication.class, args);
